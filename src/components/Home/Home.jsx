1a947233685f93d07d2229ce4ed19ab961f050fa
Franco <francovelasco10@hotmail.com>
2023-08-26 20:27:51 -0500

    Creacion

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+//Lo ideal es que este componente se componga de otros, un componente para las tendencias, otro para la tv, etc etc
+
+export default function Home() {
+	return <div>Home</div>;
+}
